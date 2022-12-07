04ddbceaf8233628641811ca4c4259f127d34fc6
Gopinath <Pratheepa.Gopinath.-ND@disney.com>
2022-12-07 18:14:00 +0530

    redis template sample

---
+++ src/test/java/com/example/redis11/Redis11ApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.redis11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Redis11ApplicationTests {
+
+    /*
+     * @Test void contextLoads() { }
+     */
+}
